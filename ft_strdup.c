f787dd628485bad966a189121ff108f302f0b2c4
Baobe <wmmxxbb@gmail.com>
2019-10-11 01:07:43 +0200

    strdup

---
+++ ft_strdup.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
+
+size_t		get_lens(char *s)
+{
+	size_t i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char		*strdup(const char *s)
+{
+	size_t	lens;
+	size_t	i;
+	char 	*str;
+	char *s1;
+
+	i = 0;
+	s1 = (char *)s;
+	str = (char *)malloc(lens + 1);
+	while (i < lens)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
