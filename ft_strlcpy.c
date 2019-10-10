497307d2ec69e9f4010c58d26a2a8c834dc04201
Baobe <wmmxxbb@gmail.com>
2019-10-10 16:03:33 +0200

    rewrite strlcpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <stdio.h>
+
+size_t		ft_get_strlens(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+size_t		strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < size - 1 && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (ft_get_strlens((char *)src));
+}
