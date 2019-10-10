a7974cb5a3a4062dcdf15b4bcfc74ea87c3b941b
Baobe <wmmxxbb@gmail.com>
2019-10-10 15:26:54 +0200

    rewrite memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+
+int ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char *str1;
+	unsigned char *str2;
+	unsigned int i;
+
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
+		i++;
+	}
+	return (0);
+}
