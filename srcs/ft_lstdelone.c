1046d7cb719546f584dfbee53f29eb2402d6765f
Baobe <wmmxxbb@gmail.com>
2019-10-17 02:58:10 +0200

    4

--- srcs/ft_lstdelone.c
+++ srcs/ft_lstdelone.c
@@ -1,13 +1,11 @@
 #include "libft.h"
 
-void ft_lstdelone(t_list *lst, void (*del)(void *))
+void		ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	t_list *ptr;
-
 	if (!lst || !del)
 		return ;
-	ptr = lst;
 	(*del)(lst->content);
 	lst->next = NULL;
 	free(lst);
+	lst = NULL;
 }
