422da5a4f1d71e4aeec1a8237067ab18b8cef3b6
Baobe <wmmxxbb@gmail.com>
2019-10-18 03:01:38 +0200

    fix strtrim

--- srcs/ft_strtrim.c
+++ srcs/ft_strtrim.c
@@ -6,13 +6,24 @@
 /*   By: xinwang <xinwang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/17 18:29:43 by xinwang           #+#    #+#             */
-/*   Updated: 2019/10/17 18:29:47 by xinwang          ###   ########.fr       */
+/*   Updated: 2019/10/18 03:01:28 by xinwang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int char_in_set(char c, char *set)
+static char *empty_string(void)
+{
+	char *p;
+
+	p = (char *)malloc(sizeof(char));
+	if (!p)
+		return (NULL);
+	p[0] = '\0';
+	return (p);
+}
+
+static int char_in_set(char c, char *set)
 {
 	int i;
 
@@ -62,7 +73,7 @@ char		*ft_strtrim(char const *s1, char const *set)
 	start = get_starting_index((char *)s1, (char *)set);
 	end = get_end_start((char *)s1, (char *)set);
 	if (!(ptr = (char *)malloc(sizeof(char) * (end - start + 2))))
-		return (NULL);
+		return (empty_string());
 	i = 0;
 	while (start <= end)
 		ptr[i++] = s1[start++];
